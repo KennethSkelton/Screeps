67fbee6e0cc9b2761c92e9274ed0b1ec5a8aeb61
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-08 04:06:24 -0400

    disable cataloger

--- src/main.ts
+++ src/main.ts
@@ -30,12 +30,13 @@ function unwrappedLoop(): void {
 
 
   //Catalog Room
+  /*
   Object.values(Game.rooms).forEach(room => {
     if (room.controller?.my) {
       storeSourcesInMemory(room)
     }
   });
-
+  */
 
   Object.values(Game.creeps).forEach(creep => {
     if (creep.memory.role === 'harvester') {
