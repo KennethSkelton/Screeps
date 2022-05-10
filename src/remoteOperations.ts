3bfb3d65d2050e36d2e7c0c14a595c6c1a2c453c
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-10 16:06:20 -0400

    bugfix

--- src/remoteOperations.ts
+++ src/remoteOperations.ts
@@ -15,7 +15,7 @@ declare global {
 
 function remoteOperations(spawnName: string, operationList: { roomName: string; type: string }[]): void {
   for (const operation of operationList) {
-    if (!Memory.remoteOperations[operation.roomName]) {
+    if (!Memory.remoteOperations || !Memory.remoteOperations[operation.roomName]) {
       Memory.remoteOperations[operation.roomName] = { stage: 0, type: operation.type };
     } else {
       const operationInfo = Memory.remoteOperations[operation.roomName];
