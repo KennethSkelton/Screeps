8457b55230dcedfe097e71c0ccbc6bcd0e5265ca
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-10 16:19:23 -0400

    bugfix

--- src/remoteOperations.ts
+++ src/remoteOperations.ts
@@ -31,29 +31,30 @@ function remoteOperations(spawnName: string, operationList: { roomName: string; 
 }
 
 function remoteMine(spawnName: string, roomName: string, stage: number): void {
-  if (Game.flags[`${roomName}_Staging_Area`]) {
+  if (!Game.flags[`${roomName}_Staging_Area`]) {
     Game.rooms[roomName].createFlag(25, 25, `${roomName}_Staging_Area`);
   }
 
-  if (stage < 3) {
-    const quota: Record<string, number>[] = [
-      { scout: 1 },
-      { claimer: 1 },
-      { harvester: Game.rooms[roomName].find(FIND_SOURCES).length },
-      { remoteBuilder: 2 }
-    ];
-    spawnFromQuota(spawnName, quota, true, roomName);
-  } else if (stage < 2) {
-    const quota: Record<string, number>[] = [
-      { scout: 1 },
-      { claimer: 1 },
-      { remoteHarvester: Game.rooms[roomName].find(FIND_SOURCES).length }
-    ];
-    spawnFromQuota(spawnName, quota, true, roomName);
-  } else if (stage < 1) {
-    const quota: Record<string, number>[] = [{ scouter: 1 }];
-    spawnFromQuota(spawnName, quota, true, roomName);
-  }
+  if (Game.rooms)
+    if (stage < 3) {
+      const quota: Record<string, number>[] = [
+        { scout: 1 },
+        { claimer: 1 },
+        { harvester: Game.rooms[roomName].find(FIND_SOURCES).length },
+        { remoteBuilder: 2 }
+      ];
+      spawnFromQuota(spawnName, quota, true, roomName);
+    } else if (stage < 2) {
+      const quota: Record<string, number>[] = [
+        { scout: 1 },
+        { claimer: 1 },
+        { remoteHarvester: Game.rooms[roomName].find(FIND_SOURCES).length }
+      ];
+      spawnFromQuota(spawnName, quota, true, roomName);
+    } else if (stage < 1) {
+      const quota: Record<string, number>[] = [{ scouter: 1 }];
+      spawnFromQuota(spawnName, quota, true, roomName);
+    }
 }
 
 export { remoteMine, remoteOperations };
--- src/roles/scouter.ts
+++ src/roles/scouter.ts
@@ -9,7 +9,9 @@ interface ScouterMemory extends CreepMemory {
 const roleScouter = {
   run(creep: Scouter): void {
     if (creep.memory.targetRoom) {
-      creep.moveTo(new RoomPosition(25, 25, creep.memory.targetRoom), { visualizePathStyle: { stroke: '#ffaa00' } });
+      creep.moveTo(Game.flags[`${creep.memory.targetRoom}_Staging_Area`].pos, {
+        visualizePathStyle: { stroke: '#ffaa00' }
+      });
       if (creep.room.name == creep.memory.targetRoom) {
         if (Memory.remoteOperations[creep.memory.targetRoom].stage < 1) {
           Memory.remoteOperations[creep.memory.targetRoom].stage = 1;
