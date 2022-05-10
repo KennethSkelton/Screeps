1983560cb83a7750f1f62538649ff03e362ff8a3
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-10 01:15:31 -0400

    remove harvester console.log

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -3,7 +3,7 @@ export interface Harvester extends Creep {
 }
 
 interface HarvesterMemory extends CreepMemory {
-  role: "harvester";
+  role: 'harvester';
   sourceId?: Id<Source>;
 }
 
@@ -11,12 +11,12 @@ const roleHarvester = {
   run(creep: Harvester): void {
     if (!creep.memory.sourceId) {
       const potentialSources: Id<Source>[] = [];
-      const harvesters = _.filter(Game.creeps, (creep: Harvester) => creep.memory.role == "harvester");
+      const harvesters = _.filter(Game.creeps, (creep: Harvester) => creep.memory.role == 'harvester');
       const groupedHarvesters = _.groupBy(harvesters, function (n: Harvester) {
         return n.memory.sourceId;
       });
 
-      delete groupedHarvesters["undefined"];
+      delete groupedHarvesters['undefined'];
       for (const [sourceId, sourceInfoObject] of Object.entries(Memory.rooms[creep.room.name].sources)) {
         if (!groupedHarvesters[sourceId] || groupedHarvesters[sourceId].length < sourceInfoObject.workerSpots) {
           potentialSources.push(sourceId as Id<Source>);
@@ -28,9 +28,8 @@ const roleHarvester = {
       if (!source) {
         source = creep.room.find(FIND_SOURCES)[0];
       }
-      console.log(`${creep.name} ${source.id}`);
       if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(source, { visualizePathStyle: { stroke: "#ffaa00" } });
+        creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
       }
       creep.memory.sourceId = source.id;
     } else {
@@ -39,7 +38,7 @@ const roleHarvester = {
         if (source.energy == 0) {
           delete creep.memory.sourceId;
         } else if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(source, { visualizePathStyle: { stroke: "#ffaa00" } });
+          creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
         }
       }
     }
