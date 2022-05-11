ca15995b9fb0ab611e8338d94567dd19327c47dc
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-11 15:55:38 -0400

    profiler

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { storeSourcesInMemory } from './roomCatalog';
 import { HOME_ROOM, HOME_SPAWN, REMOTE_OPERATIONS_LIST } from './constants';
 import { assignJobs } from 'taskAssignment';
 import { remoteOperations } from 'remoteOperations';
+import profiler from 'screeps-profiler';
 
 declare global {
   interface CreepMemory {
@@ -16,7 +17,10 @@ declare global {
   }
 }
 
-function unwrappedLoop(): void {
+// eslint-disable-next-line
+profiler.enable();
+
+function main(): void {
   console.log(`Current game tick is ${Game.time}`);
 
   if (Game.spawns['Spawn1'].hits < Game.spawns['Spawn1'].hitsMax) {
@@ -60,6 +64,12 @@ function unwrappedLoop(): void {
     .forEach((name) => delete Memory.creeps[name]);
 }
 
+function unwrappedLoop(): void {
+  // eslint-disable-next-line
+  profiler.wrap(() => {
+    main();
+  });
+}
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 const loop = ErrorMapper.wrapLoop(unwrappedLoop);
