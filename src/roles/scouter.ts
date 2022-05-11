d642a4951288f945cdf70474f123fc8c126a4b59
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-11 02:26:27 -0400

    scouter find route

--- src/roles/scouter.ts
+++ src/roles/scouter.ts
@@ -4,20 +4,24 @@ export interface Scouter extends Creep {
 
 interface ScouterMemory extends CreepMemory {
   role: 'scouter';
-  path: string;
 }
 
 const roleScouter = {
   run(creep: Scouter): void {
     if (creep.memory.targetRoom) {
-      if (!creep.memory.path) {
-        const path = creep.pos.findPathTo(new RoomPosition(25, 25, creep.memory.targetRoom));
-        creep.memory.path = Room.serializePath(path);
-      }
-      creep.moveByPath(creep.memory.path);
       if (creep.room.name == creep.memory.targetRoom) {
+        creep.moveTo(new RoomPosition(25, 25, creep.room.name));
         if (Memory.remoteOperations[creep.memory.targetRoom].stage < 1) {
           Memory.remoteOperations[creep.memory.targetRoom].stage = 1;
+        }
+      } else {
+        const route = Game.map.findRoute(creep.room, creep.memory.targetRoom);
+        if (route != ERR_NO_PATH && route.length > 0) {
+          console.log('Now heading to room ' + route[0].room);
+          const exit = creep.pos.findClosestByRange(route[0].exit);
+          if (exit) {
+            creep.moveTo(exit);
+          }
         }
       }
     }
