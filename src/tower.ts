188ed11767058e71708e0b4111760a75a27e9742
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-11 13:14:37 -0400

    tower repair function

--- src/tower.ts
+++ src/tower.ts
@@ -3,20 +3,20 @@ function runTower(tower: StructureTower): void {
 
   if (closestHostile) {
     tower.attack(closestHostile);
-  } /*else {
-    const targets = tower.room.find(FIND_STRUCTURES, { filter: isDamaged })
-    targets.sort((a,b) => a.hits - b.hits);
+  } else if (tower.room.energyAvailable / tower.room.energyCapacityAvailable > 0.5) {
+    const targets = tower.room.find(FIND_STRUCTURES, { filter: isDamaged });
+    targets.sort((a, b) => a.hits - b.hits);
     if (targets.length > 0) {
       tower.repair(targets[0]);
     }
-  }*/
+  }
 }
 
 function isDamaged(structure: Structure): boolean {
+  if (structure.structureType != STRUCTURE_RAMPART) {
+    return false;
+  }
   return structure.hits < structure.hitsMax;
 }
 
-export {
-  isDamaged,
-  runTower
-};
+export { isDamaged, runTower };
