30cd2cdb7ec5ac972ff296d2a1e53233cf6f28f9
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-12 14:52:11 -0400

    hauler change

--- src/roles/hauler.ts
+++ src/roles/hauler.ts
@@ -21,7 +21,7 @@ const roleHauler = {
               if (creep.memory.path[0]) {
                 console.log('MOVE 1');
                 console.log(`Path object is: ${JSON.stringify(creep.memory.path[0])} `);
-                console.log(`Direction is ${creep.pos.getDirectionTo(creep.memory.path[0])}`);
+                console.log(`Direction is ${creep.pos.getDirectionTo(creep.memory.path[0].x, creep.memory.path[0].y)}`);
                 console.log(creep.move(creep.pos.getDirectionTo(creep.memory.path[0])));
               } else {
                 delete creep.memory.path;
