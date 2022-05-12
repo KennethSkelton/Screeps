d87b784835dfa4fb18e8d8f7be8c4b1a61504d69
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-12 14:27:32 -0400

    hauler fix

--- src/roles/hauler.ts
+++ src/roles/hauler.ts
@@ -54,7 +54,39 @@ const roleHauler = {
                 delete creep.memory.path;
               }
             } else {
-              creep.memory.path = PathFinder.search(creep.pos, target.pos).path;
+              creep.memory.path = PathFinder.search(creep.pos, target.pos, {
+                // We need to set the defaults costs higher so that we
+                // can set the road cost lower in `roomCallback`
+                plainCost: 2,
+                swampCost: 10,
+                roomCallback: function (roomName) {
+                  const room = Game.rooms[roomName];
+                  // In this example `room` will always exist, but since
+                  // PathFinder supports searches which span multiple rooms
+                  // you should be careful!
+                  if (!room) return false;
+                  const costs = new PathFinder.CostMatrix();
+
+                  room.find(FIND_STRUCTURES).forEach(function (struct) {
+                    if (struct.structureType === STRUCTURE_ROAD) {
+                      // Favor roads over plain tiles
+                      costs.set(struct.pos.x, struct.pos.y, 1);
+                    } else if (
+                      struct.structureType !== STRUCTURE_CONTAINER &&
+                      (struct.structureType !== STRUCTURE_RAMPART || !struct.my)
+                    ) {
+                      // Can't walk through non-walkable buildings
+                      costs.set(struct.pos.x, struct.pos.y, 0xff);
+                    }
+                  });
+
+                  // Avoid creeps in the room
+                  room.find(FIND_CREEPS).forEach(function (creep) {
+                    costs.set(creep.pos.x, creep.pos.y, 0xff);
+                  });
+                  return costs;
+                }
+              }).path;
             }
           }
         } else {
