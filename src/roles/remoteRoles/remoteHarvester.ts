115f0c0a081f77ead19614b026346130cec4858e
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-13 02:37:14 -0400

    harvester fix

--- src/roles/remoteRoles/remoteHarvester.ts
+++ src/roles/remoteRoles/remoteHarvester.ts
@@ -6,7 +6,7 @@ export interface RemoteHarvester extends Creep {
 
 export interface RemoteHarvesterMemory extends CreepMemory {
   role: 'remoteHarvester';
-  sourceId?: Id<Source>;
+  sourceId?: Id<_HasId>;
   path?: RoomPosition[];
 }
 
@@ -18,44 +18,40 @@ const roleRemoteHarvester = {
       });
     } else {
       if (!creep.memory.sourceId) {
-        const potentialSources: Id<Source>[] = [];
         const harvesters = _.filter(Game.creeps, (creep: RemoteHarvester) => creep.memory.role == 'remoteHarvester');
         const groupedHarvesters = _.groupBy(harvesters, function (n: RemoteHarvester) {
           return n.memory.sourceId;
         });
 
         delete groupedHarvesters['undefined'];
-        for (const [sourceId, sourceInfoObject] of Object.entries(Memory.rooms[creep.room.name].sources)) {
-          if (!groupedHarvesters[sourceId] || groupedHarvesters[sourceId].length < sourceInfoObject.workerSpots) {
-            potentialSources.push(sourceId as Id<Source>);
-            //break;
-          }
+        if (Memory.rooms[creep.room.name].sources) {
+          Memory.rooms[creep.room.name].sources?.forEach(function (source) {
+            if (!groupedHarvesters[source.id] || groupedHarvesters[source.id].length < source.workerSpots) {
+              creep.memory.sourceId = source.id;
+            }
+          });
         }
-        let source = Game.getObjectById(potentialSources[0]);
-
-        if (!source) {
-          source = creep.room.find(FIND_SOURCES)[0];
-        }
-        const harvestResult = creep.harvest(source);
-        if (harvestResult === ERR_NOT_IN_RANGE) {
-          move(creep, source.pos);
-        } else if (harvestResult === OK) {
-          creep.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
-        }
-        creep.memory.sourceId = source.id;
-      } else {
-        const source = Game.getObjectById(creep.memory.sourceId);
-        if (source) {
-          if (source.energy == 0) {
-            delete creep.memory.sourceId;
-          } else {
-            const harvestResult = creep.harvest(source);
-            if (harvestResult === ERR_NOT_IN_RANGE) {
+        if (creep.memory.sourceId) {
+          const source = Game.getObjectById(creep.memory.sourceId);
+          if (source instanceof Source) {
+            if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
               move(creep, source.pos);
-            } else if (harvestResult === OK) {
-              creep.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
             }
           }
+        } else {
+          creep.memory.sourceId = creep.room.find(FIND_SOURCES)[0].id;
+        }
+      } else {
+        const source = Game.getObjectById(creep.memory.sourceId);
+        if (source instanceof Source && source.energy > 0) {
+          const error = creep.harvest(source);
+          if (error === ERR_NOT_IN_RANGE) {
+            move(creep, source.pos);
+          } else if (error === OK) {
+            creep.room.createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_CONTAINER);
+          }
+        } else {
+          delete creep.memory.sourceId;
         }
       }
     }
