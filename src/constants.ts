8718bf83f0cdd1a3748c69d625a3d0c39ee5f603
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-30 19:21:59 -0400

    colonization efforts over

--- src/constants.ts
+++ src/constants.ts
@@ -125,10 +125,10 @@ const RETRIEVE_PRIORITY = [STRUCTURE_STORAGE, STRUCTURE_CONTAINER];
 const REMOTE_OPERATIONS_LIST = [
   { roomName: 'W11N56', type: 'remoteMine' },
   //{ roomName: 'W13N56', type: 'remoteRaid' }
-  { roomName: 'W12N55', type: 'remoteMine' },
+  { roomName: 'W12N55', type: 'remoteMine' }
   //{ roomName: 'W13N57', type: 'remoteRaid' }
   //{ roomName: 'W17N59', type: 'remoteRaid' }
-  { roomName: 'W17N59', type: 'colonize' }
+  //{ roomName: 'W17N59', type: 'colonize' }
 ];
 
 export {
