525b7ca5c7eaef98c6d10cdecca6272e29d341fe
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-29 23:45:44 -0400

    claimer targeting fix

--- src/roles/remoteRoles/claimer.ts
+++ src/roles/remoteRoles/claimer.ts
@@ -6,6 +6,7 @@ export interface Claimer extends Creep {
 
 interface ClaimerMemory extends CreepMemory {
   role: 'claimer';
+  target?: Id<_HasId>;
   path?: RoomPosition[];
 }
 
@@ -13,13 +14,16 @@ const roleClaimer = {
   run(creep: Claimer): void {
     if (creep.memory.targetRoom) {
       if (Game.rooms[creep.memory.targetRoom]) {
-        const target = Game.rooms[creep.memory.targetRoom].controller;
-        if (target) {
-          if (creep.reserveController(target) === ERR_NOT_IN_RANGE) {
-            move(creep, target.pos);
-          } else {
-            if (Memory.remoteOperations[creep.memory.targetRoom].stage < 2) {
-              Memory.remoteOperations[creep.memory.targetRoom].stage = 2;
+        creep.memory.target = Game.rooms[creep.memory.targetRoom].controller?.id;
+        if (creep.memory.target) {
+          const target = Game.getObjectById(creep.memory.target);
+          if (target && target instanceof StructureController) {
+            if (creep.reserveController(target) === ERR_NOT_IN_RANGE) {
+              move(creep, target.pos);
+            } else {
+              if (Memory.remoteOperations[creep.memory.targetRoom].stage < 2) {
+                Memory.remoteOperations[creep.memory.targetRoom].stage = 2;
+              }
             }
           }
         }
