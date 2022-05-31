22c2991fa307072e823d958aae2bb4978b74e56f
Kenneth Skelton <kskelto1@stevens.edu>
2022-05-31 02:22:29 -0400

    remote operation fix

--- src/remoteOperations.ts
+++ src/remoteOperations.ts
@@ -28,7 +28,7 @@ function remoteOperations(spawnName: string, operationList: { roomName: string; 
     }
     for (const operation of operationList) {
       if (!Memory.remoteOperations[operation.roomName]) {
-        Memory.remoteOperations[operation.roomName + operation.type] = { stage: 0, type: operation.type };
+        Memory.remoteOperations[operation.roomName] = { stage: 0, type: operation.type };
       } else {
         const operationInfo = Memory.remoteOperations[operation.roomName];
         if (operationInfo.type === 'remoteMine') {
