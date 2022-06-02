38ad3ee2debd4dbced1b68793d4ab9dd5306885a
Kenneth Skelton <kskelto1@stevens.edu>
2022-06-02 17:02:32 -0400

    base layouts file

---
+++ src/baseLayouts.ts
@@ -0,0 +1,22 @@
+//stamp layouts
+
+const headquartersSchema = {
+  1: ['xxxxxxx', 'xxxxxxx', 'xxxxxxx', 'xxxxxxx', 'xxxxxxx', 'xxxsxxx', 'xxxxxxx'],
+  2: ['xrrrrrx', 'rxxxxxr', 'rxxxxxr', 'rxxxxxr', 'rxxexxr', 'reeseer', 'xrrrrrx'],
+  3: ['xrrrrrx', 'rxxxxxr', 'rexxxxr', 'rxecexr', 'rexexer', 'reeseer', 'xrrrrrx'],
+  4: ['xrrrrrx', 'reexxxr', 'rexxxxr', 'rxecegr', 'rexexer', 'reeseer', 'xrrrrrx'],
+  5: ['xrrrrrx', 'reexxxr', 'rexlxxr', 'rxecegr', 'rexexer', 'reeseer', 'xrrrrrx'],
+  6: ['xrrrrrx', 'reexxtr', 'rexlxxr', 'rxecegr', 'rexexer', 'reeseer', 'xrrrrrx'],
+  7: ['xrrrrrx', 'reesftr', 'rexlxxr', 'rxecegr', 'rexexer', 'reeseer', 'xrrrrrx'],
+  8: ['xrrrrrx', 'reesftr', 'rexlxpr', 'rsecegr', 'rexexer', 'reeseer', 'xrrrrrx']
+};
+
+const labSchema = {
+  6: ['xbbr', 'xbrx', 'xrxx', 'rxxx'],
+  7: ['xbbr', 'xbrb', 'xrbb', 'rxxx'],
+  8: ['nbbr', 'bbrb', 'brbb', 'rbbo']
+};
+
+const extensionCellSchema = {
+  1: ['xxrxx', 'xrerx', 'reeer', 'xrerx', 'xxrxx']
+};
