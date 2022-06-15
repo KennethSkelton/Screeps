707fc2499d59d40d679abded97b6b6a6e5ff67fa
Kenneth Skelton <kskelto1@stevens.edu>
2022-06-15 18:42:07 -0400

    sell it all

--- src/roles/runner.ts
+++ src/roles/runner.ts
@@ -19,7 +19,7 @@ const roleRunner = {
       const terminal = terminals[0];
       const storage = storages[0];
       if (terminal instanceof StructureTerminal && storage instanceof StructureStorage) {
-        if (terminal.store[RESOURCE_ENERGY] >= 300000 || storage.store.getUsedCapacity(RESOURCE_ENERGY) < 500000) {
+        if (terminal.store[RESOURCE_ENERGY] >= 300000 /*|| storage.store.getUsedCapacity(RESOURCE_ENERGY) < 500000*/) {
           if (creep.pos.x != 36 || creep.pos.y != 37) {
             move(creep, new RoomPosition(36, 37, creep.memory.homeroom), 0);
           }
